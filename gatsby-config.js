277c2a034603a13be1571938d042f948365608ed
Andrew Miller <andrew.miller@founders4schools.org.uk>
2019-07-21 21:23:42 +0100

    update envars

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,8 +9,8 @@ module.exports = {
         resolve: 'gatsby-source-s3',
         options: {
           aws: {
-            accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+            accessKeyId: process.env.AWS_ACCESS_KEY,
+            secretAccessKey: process.env.AWS_SECRET_ACCESS,
           },
           buckets: ['thec3-teaching-series'],
         },
