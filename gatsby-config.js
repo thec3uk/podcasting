3bad29eedc2eb9a00870b9aa70a59a5def446202
Andrew Miller <andrew.miller@founders4schools.org.uk>
2019-07-23 09:56:14 +0100

    add json data to add in title and description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,45 @@
+require('dotenv').config({
+  path: '.env',
+});
+
+const AWS = require('aws-sdk');
+
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
+
+const getValidFiles = function(metadata) {
+  file_keys = ['File_1', 'File_2', 'File_3', 'File_4', 'File_5']
+  return file_keys.map(key =>{
+    if (metadata[key] != "" && metadata[key].endsWith('.mp3')) {
+      return metadata[key]
+    }}
+  )
+}
+
 module.exports = {
   siteMetadata: {
-    title: `Podcast Manager`,
-    description: `Website to create & manage podcast feeds`,
-    author: `@nanorepublica`,
+    title: `The C3 Audio Podcast`,
+    description: `Welcome to the audio podcast feed of The C3 Church. To find out more visit http://www.thec3.uk`,
+    author: `The C3 Church`,
   },
   plugins: [
+      {
+          resolve:`gatsby-transformer-json`,
+          options: {
+              typeName: ({ node, object, isArray }) => {
+                  return node.extension
+              },
+          }
+      },
+      {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `metadata`,
+        path: `${__dirname}/data/`,
+      },
+    },
         {
         resolve: 'gatsby-source-s3',
         options: {
@@ -26,18 +61,32 @@ module.exports = {
                   description
                 }
               }
-}
-`,
+            }`,
           feeds: [
             {
-              serialize: ({ query: { site, allS3Object } }) => {
-                return allS3Object.edges.map(({node}) => {
-                  return {
-                    title: 'PREACH',
-                    description: 'A C3 Preach',
-                    date: new Date(node.Key.split('/').splice(0,3).join('-')),
-                    url: `https://${node.Name}.s3.amazonaws.com/${node.Key}`,
-                    guid: `https://${node.Name}.s3.amazonaws.com/${node.Key}`,
+              serialize: ({ query: { site, allS3Object, allJson } }) => {
+                return allJson.edges.map(({node}) => {
+                  S3Obj = allS3Object.edges.filter((s3node) => {
+                    var s3node = s3node.node
+                    // get a list of files from the JSON blob
+                    validMediaFiles = getValidFiles(node)
+                    //
+                    var thing = validMediaFiles.filter(file =>
+                      s3node.Key.endsWith(file)
+                    )
+                    return thing[0]
+                  })[0]
+                  if (S3Obj !== undefined) {
+                    var S3ObjNode = S3Obj.node
+                    var s3URL = `https://${S3ObjNode.Name}.s3.amazonaws.com/${S3ObjNode.Key}`
+
+                    return {
+                      title: node.Recording_Title,
+                      description: node.Description,
+                      date: new Date(S3ObjNode.Key.split('/').splice(0,3).join('-')),
+                      url: s3URL,
+                      guid: s3URL,
+                    }
                   }
                 })
               },
@@ -53,6 +102,22 @@ module.exports = {
                       }
                     }
                 }
+                allJson {
+    edges {
+      node {
+        Bible_References
+        Description
+        Recorded_Date
+        Recording_Title
+        Speaker
+        File_1
+        File_2
+        File_3
+        File_4
+        File_5
+      }
+    }
+  }
                 }
               `,
               output: "/feed.xml",
